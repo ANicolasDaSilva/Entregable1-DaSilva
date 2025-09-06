69bf3cd070d35736195e8fae9f73de7767544771
chimpa27 <dasilva.an97@gmail.com>
2025-09-06 05:00:26 -0300

    primer commit js

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Bienvenida persona curiosa");
+
+let nombre = prompt("Perdon, primero los modales ¿como te llamas?");
+
+let edadIngresada = prompt("¿Y que edad tienes?");
+
+alert(`Hola ${nombre} de ${edadIngresada} años`);
