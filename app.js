db5433d56112239c301e8d1b8d67f6b9f79d4081
chimpa27 <dasilva.an97@gmail.com>
2025-09-06 10:52:11 -0300

    ciclos, condicional

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 alert("Bienvenida persona curiosa");
 
 let nombre = prompt("Perdon, primero los modales ¿como te llamas?");
-
 let edadIngresada = prompt("¿Y que edad tienes?");
+edadIngresada = Number(edadIngresada);
 
-alert(`Hola ${nombre} de ${edadIngresada} años`);
+//condicional if
+if (nombre === "" || nombre === null) {
+  alert("NO ingresaste tu nombre, con esa actitud no pasaras.");
+}else if (edadIngresada < 18 || edadIngresada === "" || edadIngresada === null || isNaN(edadIngresada)) {
+  alert("Sos menor de edad, no podes pasar");
+}else if (edadIngresada >= 18 || edadIngresada === "" || edadIngresada === null || isNaN(edadIngresada)) {
+  alert("Mmm medio justo de edad");
+}else {
+  alert(`Hola ${nombre} de ${edadIngresada} años`);
+}
+
+//ciclo
+let tabla = parseInt(prompt("Igual para aprender no hace falta, ¿que tabla de multiplicar queres aprender?"));
+
+for (let i = 1; i <= 10; i++) {
+  let resultado = tabla * i;
+  if (tabla === "" || tabla === null || isNaN(tabla)) {
+    alert("Por favor, ingresa un número.");
+  } else {
+    alert(tabla + ` x ` + i + ` = ` + resultado);
+  }
+}
+
+const contraseña = "123";
+let intentoContra = prompt("Ingrese la contraseña");
+
+while (intentoContra != contraseña){
+  intentoContra = prompt("Ingrese la contraseña");
+}
+
+//intentar aplicar una funcion con el while para la prox
+//probar metodo de busqueda con inventario de kiosco 
